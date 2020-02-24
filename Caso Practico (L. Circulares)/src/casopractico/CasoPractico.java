2164b66bad16e43af602174184897476a315993e
SebasG25 <53189683+SebasG25@users.noreply.github.com>
2020-02-23 21:05:10 -0500

    Caso Práctuco (Listas Circulares)

--- Caso Practico (L. Circulares)/src/casopractico/CasoPractico.java
+++ Caso Practico (L. Circulares)/src/casopractico/CasoPractico.java
@@ -6,6 +6,8 @@
 package casopractico;
 import java.util.Scanner;
 import java.awt.BorderLayout;
+import java.util.InputMismatchException;
+
 
 /**
  *
@@ -13,25 +15,106 @@ import java.awt.BorderLayout;
  */
 public class CasoPractico {
 
+    
     /**
      * @param args the command line arguments
+     * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
+        CircularDoublyLinkedList<Video> listaCircular = new CircularDoublyLinkedList<>();
+        System.out.println("\033[31mLista de reproducción de video");
         Scanner entrada = new Scanner(System.in);
-        int opcion;
-        System.out.println("Lista de reproducción de video");
-            System.out.println("1. Agregar video");
-            System.out.println("2. Eliminar");
-            System.out.println("3. Actualizar");
-            System.out.println("4. Ver lista");
-            System.out.println("5. Reproducción automática");
-            System.out.println("6. Salir");
+        String opcion;
         do{
-            
+            System.out.println();
+            System.out.println("\033[31m1. \033[30mAgregar video");
+            System.out.println("\033[31m2. \033[30mEliminar");
+            System.out.println("\033[31m3. \033[30mActualizar");
+            System.out.println("\033[31m4. \033[30mVer lista");
+            System.out.println("\033[31m5. \033[30mReproducción automática");
+            System.out.println("\033[31m6. \033[30mSalir");  
             System.out.print("Ingrese una opcion de 1 a 6: ");
-            opcion = entrada.nextInt();
-        }while(opcion == 6);
+            
+            opcion = entrada.nextLine();
+            if(opcion.equals("")){
+                System.out.println("\033[31mERROR: \033[30mPor favor ingresa una opción");
+            }else if(opcion.equals("1")){
+                addVideo(listaCircular);
+            }else if (opcion.equals("2")){
+                deleteVideo(listaCircular);
+            }else if(opcion.equals("3")){
+                try{
+                    updateVideo(listaCircular);
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }else if(opcion.equals("4")){
+                showList(listaCircular);
+            }else if(opcion.equals("5")){
+                playSongs(listaCircular);
+            }else{
+                System.out.println("\033[31mERROR: \033[30mPor favor ingresa una opción válida");
+            }
+        }while(!opcion.equals("6"));
+                
     }
     
+    public static void addVideo(CircularDoublyLinkedList lista){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese nombre del video: ");
+        String videoName = entrada.nextLine();
+        lista.add(videoName);
+        System.out.println();
+    }
+    
+    public static void deleteVideo(CircularDoublyLinkedList lista){
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese nombre del video a borrar: ");
+        String videoName = entrada.nextLine();
+        try {
+            lista.deleteVideo(videoName);
+            System.out.println("\033[31mVideo eliminado con éxito");
+        } catch (Exception e) {
+            System.out.println("\033[31mERROR: \033[30m" + e.getMessage());
+        }
+    }
+    
+    public static void updateVideo(CircularDoublyLinkedList lista) throws Exception{
+        Scanner entrada = new Scanner(System.in);
+        
+        String videoName = "";
+        while(videoName.isEmpty()){
+            System.out.print("Ingrese nombre del video a actualizar: ");
+            videoName = entrada.nextLine();
+            videoName = videoName.trim();
+        }
+        
+        if(lista.checkData(videoName)){
+            String newName = "";
+            while(newName.isEmpty()){
+                System.out.print("Ingrese nombre del nuevo video: ");
+                newName = entrada.nextLine();
+                newName = newName.trim();
+            }
+            
+            try {
+                lista.updateVideo(videoName, newName);
+            } catch (Exception e) {
+                System.out.println("\033[31mERROR: \033[30m" + e.getMessage());
+            }
+        }else{
+            System.out.println("\033[31mERROR: \033[30mNo se encuentra el video ingresado");
+        }
+        
+    }
+    
+    public static void playSongs(CircularDoublyLinkedList lista) throws InterruptedException{
+        lista.play();
+    }
+    
+    public static void showList(CircularDoublyLinkedList lista){
+        System.out.println("\033[31m\nLista de reproducción: \033[30m" + lista.showData());
+    }
 }
+    
--- Caso Practico (L. Circulares)/src/casopractico/CircularDoublyLinkedList.java
+++ Caso Practico (L. Circulares)/src/casopractico/CircularDoublyLinkedList.java
@@ -5,9 +5,11 @@
  */
 package casopractico;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Sebastian
+ * @author Sebastián Guzmán
  */
 public class CircularDoublyLinkedList<T> {
      Video<T> head;
@@ -43,8 +45,6 @@ public class CircularDoublyLinkedList<T> {
             newVideo.setNextVideo(head);
             head.getPreviousVideo().setNextVideo(newVideo);
             head.setPreviousVideo(newVideo);
-            //head=newNode;
-            //¿Se puede refactorizar?
         }
     }
 
@@ -59,6 +59,66 @@ public class CircularDoublyLinkedList<T> {
             head = head.getNextVideo();
         }
     }
+    
+    public void deleteVideo(T videoName)throws Exception{
+        if (isEmpty() || !checkData(videoName)) {
+            throw new Exception("No se encontró el video ingresado");
+        } else {
+            Video<T> current = this.head;
+            do{ 
+                if(checkData(videoName)){
+                    System.out.println("ENCONTRADO");
+//                    if(current.getName() == head.getName() && head.getName() == head.getPreviousVideo().getName()){
+//                        head = null;
+//                    }else{
+//                       current.getPreviousVideo().setNextVideo(current.getNextVideo());
+//                       current.getNextVideo().setPreviousVideo(current.getPreviousVideo()); 
+//                    }
+                }
+                current = current.getNextVideo();
+            }while(current != this.head);
+        }
+    }
+    
+    public void updateVideo(T videoName, T newName)throws Exception{
+        if (isEmpty() || !checkData(videoName)) {
+            throw new Exception("\033[31mError: \033[30mNo se encontró el video ingresado");
+        } else {
+            Video<T> current = this.head;
+            do{
+                if(checkData(videoName)){
+                    current.setName(newName);
+                    System.out.println("\033[31mCanción actualizada con éxito");
+                }
+            }while(current != this.head);
+        }
+    }
+    
+    public void play() throws InterruptedException{
+        if(isEmpty()){
+            System.out.println("\033[31mNo hay videos en la lista para reproducir");
+        }else{
+            Video<T> current = this.head;
+            do {
+                System.out.println("\033[31m\nReproduciendo: " + "\033[30m" + current.getName() + "\n");
+                Thread.sleep(2000);
+                current = current.getNextVideo();
+            }while (current != head);
+            
+            Scanner entrada = new Scanner(System.in);
+            String answer = "";
+            while(answer.isEmpty() || answer.equalsIgnoreCase("no") || !answer.equalsIgnoreCase("si")){
+                System.out.println("¿Desea repetir la lista de reproducción? Si/No");
+                answer = entrada.nextLine();
+                answer = answer.trim();
+                if(answer.equalsIgnoreCase("si")){
+                    play();
+                }else if(answer.equalsIgnoreCase("no")){
+                    break;
+                }
+            }
+        }
+    }
 
     public void deleteLast() throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -69,26 +129,37 @@ public class CircularDoublyLinkedList<T> {
     }
 
     public String showData() {
-        String data = "";
-        Video<T> current = this.head;
-        do {
-            data = data + current.getName() + " ";
-            current = current.getNextVideo();
-        } while (current != head);
+        String data = "\n";
+        if(isEmpty()){
+            data = "No hay videos en la lista";
+        }else{
+            int cont = 1;
+            Video<T> current = this.head;
+            do {
+                data += "\033[31m" + cont + ". \033[30m" + current.getName() + "\n";
+                current = current.getNextVideo();
+                cont++;
+            }while (current != head);
+            return data;
+        }
         return data;
     }
     
-    public boolean checkData(T name) {
+    public boolean checkData(T name) throws Exception{
         /*
             Se recorre la lista y se encuentra un nodo con su mismo dato, retorna verdadero
         */
-        Video<T> current = this.head;
-        do{
-            if(current.getName().equals(name)){
-                return true;
-            }
-            current = current.getNextVideo();
-        }while(current != this.head);
-        return false;
+        if(isEmpty()){
+            throw new Exception("\033[31mError: \033[30mNo hay videos en la lista");
+        }else{
+            Video<T> current = this.head;
+            do{ 
+                if(current.getName().toString().equalsIgnoreCase(name.toString())){
+                    return true;
+                }
+                current = current.getNextVideo();
+            }while(current != this.head);
+            return false;
+        }
     }
 }
